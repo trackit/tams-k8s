bbdc0a13e3fc3179fe894d31380333358ab3c25f
ClarisseEynard <clarisse.eynard@epitech.eu>
2025-11-12 16:31:03 +0100

    fix: set up for dynamo tests

--- service/src/repository/builder.ts
+++ service/src/repository/builder.ts
@@ -25,7 +25,6 @@ export class RepositoriesBuilder implements Factory {
       default:
         throw new Error(`Unknown configuration database type: "${config.type}"`);
     }
-    this.initialize();
   }
 
   async initialize() {
--- service/src/repository/dynamodb/flowDeleteRequests.test.ts
+++ service/src/repository/dynamodb/flowDeleteRequests.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test } from "vitest";
+import { beforeAll, beforeEach, describe, expect, test } from "vitest";
 import { RepositoriesBuilder } from "../builder";
 import { BackendManager } from "../../backend/manager";
 import { setUpApp } from "../../setUpApp";
@@ -8,11 +8,7 @@ import { clearDynamoTable } from "utils/dynamo";
 
 const TABLE_NAME = "k8s-tams-test-flow-delete-requests";
 
-beforeEach(async () => {
-  await clearDynamoTable(TABLE_NAME, "us-west-2", "http://localhost:8000");
-});
-
-const setUp = () => {
+const setUp = async () => {
   const repository = new RepositoriesBuilder({
     type: "dynamodb",
     flowTableName: "k8s-tams-test",
@@ -27,13 +23,22 @@ const setUp = () => {
     repository,
     new BackendManager([{ type: "memory", id: "memory", default: true }]),
   );
+  await repository.initialize();
   return { app, flowDeleteRequestRepository: repository.getFlowDeleteRequestsRepository() };
 };
+
+beforeAll(async () => {
+  await setUp();
+});
+
+beforeEach(async () => {
+  await clearDynamoTable(TABLE_NAME, "us-west-2", "http://localhost:8000");
+});
 
 describe("Testing using DynamoDB repository", () => {
   describe("all flowDeleteRequest", () => {
     test("should return an empty list if no delete-request is found", async () => {
-      const { app } = setUp();
+      const { app } = await setUp();
       const response = await request(app).get("/flow-delete-requests");
 
       expect(response.status).toBe(200);
@@ -41,7 +46,7 @@ describe("Testing using DynamoDB repository", () => {
     });
 
     test("should return at least one stored delete-requests", async () => {
-      const { app, flowDeleteRequestRepository } = setUp();
+      const { app, flowDeleteRequestRepository } = await setUp();
       const request1 = FlowDeleteRequestMother.created()
         .withId("11111111-1111-1111-1111-111111111111")
         .withTimerangeToDelete("0:10_")
@@ -74,7 +79,7 @@ describe("Testing using DynamoDB repository", () => {
 
   describe("flowDeleteRequest by id", () => {
     test("should return status code 404 if delete-request doesn't exist", async () => {
-      const { app } = setUp();
+      const { app } = await setUp();
       const response = await request(app).get(
         "/flow-delete-requests/fcbef7e2-a6b2-486d-8f4e-a408504afcf9",
       );
@@ -82,7 +87,7 @@ describe("Testing using DynamoDB repository", () => {
     });
 
     test("should return the flowDeleteRequest if present", async () => {
-      const { app, flowDeleteRequestRepository } = setUp();
+      const { app, flowDeleteRequestRepository } = await setUp();
       await flowDeleteRequestRepository.saveFlowDeleteRequest(
         FlowDeleteRequestMother.created()
           .withId("fcbef7e2-a6b2-486d-8f4e-a408504afcf9")
